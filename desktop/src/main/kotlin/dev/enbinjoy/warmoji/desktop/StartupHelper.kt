ed53cf1a9ba6c8a6faeba19ca3a4a8399a16f892
Bin Zhang <ebnbin@gmail.com>
2024-12-07 01:58:00 -0800

    Remove Graal Native Image support

--- desktop/src/main/kotlin/dev/enbinjoy/warmoji/desktop/StartupHelper.kt
+++ desktop/src/main/kotlin/dev/enbinjoy/warmoji/desktop/StartupHelper.kt
@@ -77,11 +77,6 @@ class StartupHelper private constructor() {
                 return false
             }
 
-            // There is no need for -XstartOnFirstThread on Graal native image
-            if (System.getProperty("org.graalvm.nativeimage.imagecode", "").isNotEmpty()) {
-                return false
-            }
-
             val pid = LibC.getpid()
 
             // check whether -XstartOnFirstThread is enabled
