14c59722b0a93450a5b3bf1a300eeaae4810df10
Bin Zhang <ebnbin@gmail.com>
2024-12-07 04:52:50 -0800

    Update IOSLauncher

--- ios/src/main/kotlin/dev/enbinjoy/warmoji/ios/IOSLauncher.kt
+++ ios/src/main/kotlin/dev/enbinjoy/warmoji/ios/IOSLauncher.kt
@@ -1,29 +1,29 @@
-@file:JvmName("IOSLauncher")
-
 package dev.enbinjoy.warmoji.ios
-
-import org.robovm.apple.foundation.NSAutoreleasePool
-import org.robovm.apple.uikit.UIApplication
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration
+import com.badlogic.gdx.graphics.glutils.HdpiMode
 import dev.enbinjoy.warmoji.WarMoji
+import org.robovm.apple.foundation.NSAutoreleasePool
+import org.robovm.apple.uikit.UIApplication
 
-/** Launches the iOS (RoboVM) application. */
 class IOSLauncher : IOSApplication.Delegate() {
     override fun createApplication(): IOSApplication {
-        return IOSApplication(WarMoji(), IOSApplicationConfiguration().apply {
-            // Configure your application here.
-        })
+        val listener = WarMoji()
+        val config = IOSApplicationConfiguration().also {
+            it.hdpiMode = HdpiMode.Pixels
+        }
+        return IOSApplication(listener, config)
     }
 
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            val pool = NSAutoreleasePool()
-            val principalClass: Class<UIApplication>? = null
-            val delegateClass = IOSLauncher::class.java
-            UIApplication.main(args, principalClass, delegateClass)
-            pool.close()
+        @JvmStatic
+        fun main(args: Array<String>) {
+            NSAutoreleasePool().use {
+                val principalClass: Class<UIApplication>? = null
+                val delegateClass = IOSLauncher::class.java
+                UIApplication.main(args, principalClass, delegateClass)
+            }
         }
     }
 }
