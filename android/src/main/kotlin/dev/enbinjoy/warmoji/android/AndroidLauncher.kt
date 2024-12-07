3b858ea2060c75a4c0a7f5e86557153a8984279a
Bin Zhang <ebnbin@gmail.com>
2024-12-07 04:45:11 -0800

    Update AndroidLauncher

--- android/src/main/kotlin/dev/enbinjoy/warmoji/android/AndroidLauncher.kt
+++ android/src/main/kotlin/dev/enbinjoy/warmoji/android/AndroidLauncher.kt
@@ -1,18 +1,15 @@
 package dev.enbinjoy.warmoji.android
 
 import android.os.Bundle
-
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import dev.enbinjoy.warmoji.WarMoji
 
-/** Launches the Android application. */
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initialize(WarMoji(), AndroidApplicationConfiguration().apply {
-            // Configure your application here.
-            useImmersiveMode = true // Recommended, but not required.
-        })
+        val listener = WarMoji()
+        val config = AndroidApplicationConfiguration()
+        initialize(listener, config)
     }
 }
