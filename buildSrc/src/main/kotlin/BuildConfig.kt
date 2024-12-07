d9c9aa94b2a40c555d4de999dbe44f479ca397de
Bin Zhang <ebnbin@gmail.com>
2024-12-07 02:40:18 -0800

    Update Java to version 21

---
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -0,0 +1,9 @@
+object BuildConfig {
+    const val JAVA_VERSION = 21
+
+    // https://github.com/adoptium/temurin21-binaries
+    const val JDK_URL_AARCH64_MAC = "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_aarch64_mac_hotspot_21.0.5_11.tar.gz"
+    const val JDK_URL_X64_MAC = "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_mac_hotspot_21.0.5_11.tar.gz"
+    const val JDK_URL_X64_LINUX = "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz"
+    const val JDK_URL_X64_WINDOWS = "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_windows_hotspot_21.0.5_11.zip"
+}
